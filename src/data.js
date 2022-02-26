39662252d3a5c74b70c207649602674b429eee1c
Emilce-Aide-Toledo <toledo.emilce.a@gmail.com>
2022-02-25 20:32:34 -0500

    modified: header

--- src/data.js
+++ src/data.js
@@ -1,17 +1,4 @@
-export const example = () => {
-  return 'example';
-};
-
-export const anotherExample = () => {
-  return 'OMG';
-};
-
 export const searchPokemon = (namePokemon, datapokemon ) => {
-  const resultPokemon = datapokemon.pokemon.filter(x => x.name === namePokemon );
+  const resultPokemon = datapokemon.pokemon.filter(x => x.name === namePokemon);
 return resultPokemon;
-};
-
-export const searchPokemonType = (typePokemon, datapokemon ) => {
-  const resultPokemontype = datapokemon.pokemon.filter(x => x.type === typePokemon );
-return resultPokemontype;
 };
--- src/main.js
+++ src/main.js
@@ -5,11 +5,6 @@ const searchWrapper = document.querySelector(".searchInput");
 const inputBox = searchWrapper.querySelector(".inputSearch");
 const dataBox = searchWrapper.querySelector(".autocom__box");
 
-function selectName(element){
-  let selectUserData= element.innerHTML;
-  inputBox.value=selectUserData;
-}
-
 inputBox.onkeyup = (e) => {
   let userData = e.target.value;
   let emptyArray = [];
@@ -20,24 +15,30 @@ inputBox.onkeyup = (e) => {
     emptyArray = emptyArray.map((pokemon) => {
       return (pokemon = `<li>` + pokemon.name + `</li>`);
     });
-    searchWrapper.classList.add("active");
+    searchWrapper.classList.add('active');
     showDataPokemon(emptyArray);
-    let allList = dataBox.querySelectorAll("li");
+    let allList = dataBox.querySelectorAll('li');
     for (let i = 0; i < allList.length; i++) {
-      allList[i].setAttribute("onclick", 'selectName(this)');
+      allList[i].setAttribute('onclick', 'selectName(this)');
     }
   } else {
-    searchWrapper.classList.remove("active");
+    searchWrapper.classList.remove('active');
   }
 };
+function selectName(element){
+  let selectUserData= element.innerHTML;
+  inputBox.value=selectUserData;
+};
+
 function showDataPokemon(list) {
   let listData;
   if (!list.length) {
     let userValue = inputBox.value;
     listData = `<li>` + userValue + `</li>`;
   } else {
-    listData = list.join("");
+    listData = list.join('');
     dataBox.innerHTML = listData;
+    console.log(listData);
   }
 }
 // console.log(example, data);
@@ -45,8 +46,9 @@ function showDataPokemon(list) {
 const btnName = document.getElementById("btnName");
 btnName.addEventListener("click", function () {
   const namePokemon = document.getElementById("namePokemon").value;
+  const lowerCaseName= namePokemon.toLowerCase();
   const result = document.getElementById("result");
-  const resultData = searchPokemon(namePokemon, datapokemon);
+  const resultData = searchPokemon(lowerCaseName, datapokemon);
   const pokemonList = resultData.map(function (pokemon) {
     return `<li class="container">
               <p class="namePokemon">${pokemon.name} </p>
@@ -60,28 +62,26 @@ btnName.addEventListener("click", function () {
               </li>`;
   });
   result.innerHTML = `${pokemonList}`;
+});
+
+btnName.addEventListener("click", function () {
+  const namePokemon = document.getElementById("namePokemon").value;
+  const lowerCaseName= namePokemon.toLowerCase();
+  const resultBackCard = document.getElementById("resultBackCard");
+  const resultDataBack = searchPokemon(lowerCaseName, datapokemon);
+  const pokemonListBack = resultDataBack.map(function (pokemon) {
+    return `<li class="backContainer">
+            <p class="namePokemon">${pokemon.name}</p>
+            <p class="namePokemon">${pokemon.num}</p>
+            <p class="typePokemon">${pokemon.type} </p>
+            <p class= "sizePokemon"> ${pokemon.size.height}  ${pokemon.size.weight}</p>
+            <div class="resistantPokemon"><p>${pokemon.resistant[0]}</p><br>
+            <div class="weaknessesPokemon"><p>${pokemon.weaknesses[0]}
+            </li>`;
+  });
+  resultBackCard.innerHTML = `${pokemonListBack}`;
   // document.getElementById('result').innerHTML=resultData[0].name;
   // document.getElementById('result__1').innerHTML=resultData[0].about;
   // document.getElementById('result__2').innerHTML=resultData[0].generation.num;
   // document.getElementById('result__3').innerHTML=resultData[0].stats['base-attack'];
-});
-
-const btnBack = document.getElementById("btnBack");
-btnBack.addEventListener("click", function () {
-  // const namePokemon = datapokemon;
-  const resultBackCard = document.getElementById("resultBackCard");
-  const resultDataPokemon = searchPokemonType(typePokemon, datapokemon );
-  const pokemonListBack = resultDataPokemon.map(function (pokemon) {
-    return `<li class="backContainer">
-              <p class="namePokemon">${pokemon.name} </p>
-              <p class="numPokemon">${pokemon.num} </p>
-              <img class= "rarityPokemon" src="${pokemon.pokemon-rarity}">
-              <p class= "typePokemon"> ${pokemon.type}</p>
-              <div class="resistantPokemon"><p>${pokemon.resistant[0, 1, 2, 3, 4]}</p><br>
-              <p> Región: ${pokemon.generation.name} </p></div>
-              <div class="statsPokemon"><p> Ataque: ${pokemon.stats["base-attack"]}</p><br>
-              <p> Defensa: ${pokemon.stats["base-defense"]}</p></div>
-              </li>`;
-  });
-  resultBackCard.innerHTML = `${pokemonListBack}`;
 });
