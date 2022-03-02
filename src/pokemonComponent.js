ee9a3dc06acbcd9f626d119be447e112b30c1d6d
Angelica Bolivar <angelica.bolivarvc@gmail.com>
2022-03-01 20:31:28 -0500

    modified data.js

--- src/pokemonComponent.js
+++ src/pokemonComponent.js
@@ -4,9 +4,9 @@ export function pokemonComponent(pokemon) {
           <p class="numPokemon">${pokemon.num} </p>
           <img class= "imgPokemon" src="${pokemon.img}">
           <p class= "aboutPokemon"> ${pokemon.about}</p>
-          <div class="generationPokemon"><p>${pokemon.generation.num}</p><br>
+          <div class="generationPokemon"><p>${pokemon.generation.num}</p>
           <p> Región: ${pokemon.generation.name} </p></div>
-          <div class="statsPokemon"><p> Ataque: ${pokemon.stats["base-attack"]}</p><br>
+          <div class="statsPokemon"><p> Ataque: ${pokemon.stats["base-attack"]}</p>
           <p> Defensa: ${pokemon.stats["base-defense"]}</p></div>
           </li>`;
 }
@@ -20,6 +20,20 @@ export function pokemonBackComponent(pokemon) {
         <p class= "sizePokemon"> height,weight ${pokemon.size.height}  ${pokemon.size.weight}</p>
         <div class="resistantPokemon"><p> resistant ${pokemon.resistant.join(' , ')}</p>
         <div class="weaknessesPokemon"><p> weaknesses ${pokemon.weaknesses.join(',')}</p>
-        <div class="weaknessesPokemon"><p> pokemon-rarity ${pokemon['pokemon-rarity']}</p>
+        <div class="rarityPokemon"><p> pokemon-rarity ${pokemon['pokemon-rarity']}</p><div>
+        </li>`;
+}
+
+export function pokemonList(pokemon) {
+    return `<li class="ListContainer">
+        <img src="${pokemon.img}" class="imagePokemon"/>
+        <div class="cardRigth">
+        <div class="titleCard">
+        <p class="nameNPokemon">${pokemon.name}</p>
+        <p class="numberPokemon">${pokemon.num}</p>
+        </div>
+        <p class="tPokemon">Type: ${pokemon.type}</p>
+        <p class="spawn-chance">Spawn-chance: ${pokemon['spawn-chance']}</p>
+        </div
         </li>`;
 }
